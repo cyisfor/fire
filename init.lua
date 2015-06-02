0f2cbb9bda0c49e21d1b1bd0f6c6e162936c8611
user <nobody>
2015-06-02 12:43:03 -0700

    auto (/extra/user/packages/git/minetest/mods/fire/init.lua) 63 184 1930

--- init.lua
+++ init.lua
@@ -1,10 +1,43 @@
-core.setting_setbool("fire",true)
+if core.setting_get("fire") == nil then
+   core.setting_setbool("fire",true)
+   core.setting_save()
+end
+
+local fires = {
+   any = core.setting_getbool("fire",false)
+}
+
+function fires:spreading(name)
+   return self[name] or self.any
+end
+
+function fires:quench(name)
+   self[name] = true
+end
+
 core.register_chatcommand("nofire", {
                              description = "Stop flames from spreading",
                              privs = {server=true},
                              func = function(name,param)
-                                core.setting_setbool("fire",false)
-                                core.chat_send_player(name,"fire has been quenched")
+                                if param == "" then
+                                   fires.any = false
+                                   core.setting_setbool("fire",false)
+                                   core.setting_save()
+                                   core.chat_send_player(name,"fire has been quenched")
+                                end
+                             end
+})
+core.register_chatcommand("fire", {
+                             param="<type>|all"
+                             description = "Selectively start fire spreading",
+                             privs = {server=true},
+                             func = function(name,param)
+                                if param == "" then
+                                   fires.burning = true
+                                   core.setting_setbool("fire",true)
+                                   core.setting_save()
+                                   core.chat_send_player(name,"fire has been quenched")
+                                end
                              end
 })
 minetest.require("fire","basic")
--- quick.lua
+++ quick.lua
@@ -14,30 +14,45 @@ local function get_area_p0p1(pos)
         return p0, p1
 end
 
-local maxNewFlames = 0x400
+local maxNewFlames = 0x1000
 
 -- Ignite neighboring nodes
-function makeQuickFlame(victims,name)
+function makeQuickFlame(victims,name,replace)
 
    local newFlames = 0
-
+   -- no indication that a block is DEactivated,
+   -- but they will be continually reactivated,
+   -- so just reset the number to 0 every
+   -- now and again, and only the active ones
+   -- will add to it.
    local function resetFlames()
       newFlames = 0
       minetest.after(2,resetFlames)
    end
    resetFlames()
 
-   name = "fire:"..name
    local png = "fire_"..name..".png"
-   function spreadFlames(p0)
+   name = "fire:"..name
+   local function spreadFlames(p0)
       if minetest.setting_getbool("fire") then
          if newFlames < maxNewFlames then
-            newFlames = newFlames + 1
+            newFlames = newFlames - 1
             local minp,maxp = get_area_p0p1(p0)
             local nearby = minetest.env:find_nodes_in_area(minp,maxp,victims)
             for n,pos in pairs(nearby) do
-               minetest.env:set_node(pos,{name=name})
+               if newFlames < maxNewFlames then
+                  newFlames = newFlames + 1
+                  minetest.env:set_node(pos,{name=name})
+               else
+                  break
+               end
             end
+         else
+            newFlames = newFlames - 1
+         end
+         if replace then
+            minetest.env:set_node(p0,{name=replace})
+         else
             minetest.env:remove_node(p0)
          end
       else
@@ -48,7 +63,7 @@ function makeQuickFlame(victims,name)
    minetest.register_node(name, {
                              description = "Fire",
                              drawtype = "glasslike",
-                             tile_images = {png}
+                             tile_images = {png},
                              light_source = 14,
                              groups = {dig_immediate=3},
                              drop = name,
@@ -58,7 +73,7 @@ function makeQuickFlame(victims,name)
    -- Ignite neighboring nodes
    minetest.register_abm(
       {
-         nodenames = {name}
+         nodenames = {name},
          interval = 1 ,
          chance = 1,
          action = spreadFlames,
@@ -66,5 +81,6 @@ function makeQuickFlame(victims,name)
 end
 
 makeQuickFlame("group:flammable","quick_flame")
-makeQuickFlame("group:water","evaporater")
-makeQuickFlame("group:lava","quencher")
+makeQuickFlame("group:water","boiling_water")
+makeQuickFlame("group:lava","quenching_lava")
+makeQuickFlame("group:liquid","dry_dust")
